1b32ca19d6018e30a8c75d1f9ff31b54708e301b
Kenneth <kennethlombardi@users.noreply.github.com>
2021-05-06 17:08:49 -0700

    Add events

---
+++ pages/events/_event.vue
@@ -0,0 +1,34 @@
+<template>
+  <main>
+    <section v-if="post">
+      <nav class="mb-8" aria-label="go back">
+        <router-back class="block" />
+      </nav>
+
+      <article>
+        <!-- <img v-if="post.cover" class="cover-image" :src="post.cover" /> -->
+        <!-- <h6 class="inline px-2 py-1 mr-1 text-sm font-medium text-white rounded-sm bg-gray">{{ post.category }}</h6> -->
+        <h1 class="">{{ post.title }}</h1>
+        <p class="mt-1 mb-8 text-primary-600 dark:text-primary-400">{{ post.objectives }}</p>
+        <nuxt-content :document="post" />
+        <div v-if="post.gallery" class="nuxt-content">
+          <img v-for="image in post.gallery" class="image" :key="image.id" :src="image" />
+        </div>
+      </article>
+    </section>
+  </main>
+</template>
+
+<script>
+export default {
+  async asyncData({ $content, params, error }) {
+    let post
+    try {
+      post = await $content('events', params.project).fetch()
+    } catch (e) {
+      error({ message: 'Event not found' })
+    }
+    return { post }
+  },
+}
+</script>
